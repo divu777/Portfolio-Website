60223c5887c9ed4e2a0b23f963095a94e0c0322a
Divakar <divakarjaiswal777@gmail.com>
2023-08-23 12:11:42 +0530

    Project new sliding animation using gsap and ScrollMagic

--- public/script.js
+++ public/script.js
@@ -59,48 +59,113 @@ navLinks.forEach(function(navLink) {
   });
 });
 
-const observor = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-});
+// const observor = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show');
+//     } else {
+//       entry.target.classList.remove('show');
+//     }
+//   });
+// });
 
-const hiddenElementsleft = document.querySelectorAll('.hiddenleft');
-hiddenElementsleft.forEach((el) => {
-  observor.observe(el);
-});
+// const hiddenElementsleft = document.querySelectorAll('.hiddenleft');
+// hiddenElementsleft.forEach((el) => {
+//   observor.observe(el);
+// });
 
-const hiddenElementsright = document.querySelectorAll('.hiddenright');
-hiddenElementsright.forEach((el) => {
-  observor.observe(el);
-});
+// const hiddenElementsright = document.querySelectorAll('.hiddenright');
+// hiddenElementsright.forEach((el) => {
+//   observor.observe(el);
+// });
 
 // Function to handle navbar visibility based on scroll direction
-function handleNavbarScroll() {
-  var previousScrollPosition = window.scrollY;
+// function handleNavbarScroll() {
+//   var previousScrollPosition = window.scrollY;
 
-  window.addEventListener('scroll', function() {
-    var currentScrollPosition = window.scrollY;
+//   window.addEventListener('scroll', function() {
+//     var currentScrollPosition = window.scrollY;
 
-    if (currentScrollPosition > previousScrollPosition) {
-      // Scrolling down
-      // Hide the navbar
-      document.getElementById('navbar').classList.add('hidden');
-    } else {
-      // Scrolling up
-      // Show the navbar
-      document.getElementById('navbar').classList.remove('hidden');
-    }
+//     if (currentScrollPosition > previousScrollPosition) {
+//       // Scrolling down
+//       // Hide the navbar
+//       document.getElementById('navbar').classList.add('hidden');
+//     } else {
+//       // Scrolling up
+//       // Show the navbar
+//       document.getElementById('navbar').classList.remove('hidden');
+//     }
 
-    previousScrollPosition = currentScrollPosition;
-  });
-}
+//     previousScrollPosition = currentScrollPosition;
+//   });
+// }
 
 // Call the function to handle navbar scroll behavior
-handleNavbarScroll();
+// handleNavbarScroll();
 
 // Your existing smooth scroll and Intersection Observer code below...
+var controller=new ScrollMagic.Controller();
+
+// var ourScene=new ScrollMagic.Scene({
+//   triggerElement: ".projectsSection"
+// })
+// .setClassToggle('.projectHead','scroll-In')
+// .addIndicators()
+// .addTo(controller);
+// var titleBlocks = document.querySelectorAll(".titleBlock");
+// titleBlocks.forEach(function(titleBlock) {
+//   var FadeScene = new ScrollMagic.Scene({
+//     triggerElement: titleBlock,
+//     reverse:false
+//   })
+//   .setClassToggle(titleBlock, 'fade-In')
+//   .addIndicators()
+//   .addTo(controller);
+// });
+
+
+
+
+var titleTween = gsap.to(".projectHeading", { x: "100%", ease: "power1.out" });
+
+// Create a ScrollMagic scene
+var titleScene = new ScrollMagic.Scene({
+    triggerElement: ".projectHead",
+    
+     // Trigger when the top of the container reaches the top of the viewport
+    duration: "100%", // Duration of the animation
+})
+.setTween(titleTween)
+.addIndicators()
+.addTo(controller);
+
+
+// Assuming you have a ScrollMagic controller instance defined as 'controller'
+
+// Create a loop to apply animation to each ".mockups" element
+document.querySelectorAll(".mockups").forEach(function(mockup) {
+  var project = mockup.closest(".cover"); // Find the closest ancestor with class "cover"
+  var slidein = gsap.from(mockup, { y: "50%" });
+
+  // Create a ScrollMagic scene for each ".mockups" element with the project as the trigger element
+  var slideinMockup = new ScrollMagic.Scene({
+    triggerElement: project, // Use the project as the trigger element
+    duration: "100%"
+  })
+    .setTween(slidein)
+    .addIndicators() // This adds indicators for debugging (remove in production)
+    .addTo(controller); // Add the scene to the ScrollMagic controller
+});
+
+
+// var tweeenn=gsap.to(".projectOne",{x:"-10%",rotation:"-90",height:"1200px"});
+
+// var titlesBlocks=new ScrollMagic.Scene({
+//   triggerElement: ".hit",
+//   duration:"900s",
+  
+// })
+// .setTween(tweeenn)
+// .addIndicators()
+// .addTo(controller);
+
